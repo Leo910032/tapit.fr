ba54bc6f94fa2fba91d560d93429de64c7941236
Leo910032 <leozul0204@gmail.com>
2025-07-01 14:08:43 +0000

    commit

--- app/[userId]/components/SaveContactButton.jsx
+++ app/[userId]/components/SaveContactButton.jsx
@@ -418,7 +418,7 @@ export default function SaveContactButton({ userId }) {
                 {/* MAIN BUTTON - Now themed with special theme support */}
                 {selectedTheme === "New Mario" ? (
                     
-<div className="userBtn relative overflow-hidden flex justify-between items-center h-16 w-full">                        {/* Mario brick background */}
+<div className="userBtn relative overflow-x-hidden overflow-y-hidden flex justify-between items-center h-16 w-full">
                         {Array(9).fill("").map((_, brick_index) => (
                             <img
                                 key={brick_index}
